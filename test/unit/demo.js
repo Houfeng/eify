5f777a704fccfc8fe5c2b1140036317ee9e8df55
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2018-02-02 14:06:34 +0800

    1.0.1

--- test/unit/demo.js
+++ test/unit/demo.js
@@ -1,9 +1,40 @@
 const assert = require('assert');
+const EventEmitter = require('../../lib');
 
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1, 2, 3].indexOf(4));
+class Tester extends EventEmitter {
+  constructor() {
+    super();
+    this.name = 'test';
+  }
+}
+
+describe('EventEmitter', function () {
+
+  it('on', function (done) {
+    const tester = new Tester();
+    tester.on('say', e => {
+      console.log('say', e);
+      assert.equal(e, 1);
+      done();
     });
+    tester.emit('say', 1);
   });
+
+  it('off', function (done) {
+    const tester = new Tester();
+    const handelr1 = e => {
+      console.log('say1', e);
+      assert.equal(e, 1);
+    };
+    const handelr2 = e => {
+      console.log('say2', e);
+      assert.equal(e, 2);
+      done();
+    };
+    tester.on('say', handelr1);
+    tester.on('say', handelr2);
+    tester.off('say', handelr1);
+    tester.emit('say', 2);
+  });
+
 });

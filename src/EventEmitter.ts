30ac39e4c313cd14f615eda3b2bd6d765a7afd3c
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2022-05-21 13:15:52 +0800

    Update README

--- src/EventEmitter.ts
+++ src/EventEmitter.ts
@@ -1,8 +1,9 @@
 import { AbstractEventEmitter } from "./AbstractEventEmitter";
 import { EventMap } from "./EventMap";
 
-export class EventEmitter<T extends EventMap = any>
-  extends AbstractEventEmitter<T> {
+export class EventEmitter<
+  T extends EventMap = any
+> extends AbstractEventEmitter<T> {
   public emit(...args: Parameters<AbstractEventEmitter<T>["emit"]>) {
     return super.emit(...args);
   }
